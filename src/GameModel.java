85a0071ecbabe064dec6b63975a53c3613304d81
DKelle <derplyfe@gmail.com>
2014-02-22 02:11:48 -0600

    updated getPlatform

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,23 +1,21 @@
+import java.util.ArrayList;
 
 public class GameModel {
 	
+//	ArrayList<Block> blockList = new ArrayList<Block>();
+	Platform p;
+//	double x = 50, y = 50, w = 50, h = 10;
 	
-	double x = 50, y = 50, w = 50, h = 10;
 	
 	
 	public GameModel() {
-
+		p = new Platform(50,50,50,10);
+		
 		//do something
 		
 	}
-	public void moveLeft(double d){
-		x -= d;
-	}
-	public void moveRight(double d){
-		x += d;
-	}
-	public void tilt(double theta ){
-		
+	public Platform getPlatform(){
+		return p;
 	}
 	
 
