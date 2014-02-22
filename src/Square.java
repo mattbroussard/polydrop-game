d007f38ebb57c4d2b2aab6098dc5c77295d1e60c
Josh Kelle <joshua.a.kelle@gmail.com>
2014-02-22 17:44:42 -0600

    fixes to Platform. Square is new.

--- src/Square.java
+++ src/Square.java
@@ -3,30 +3,27 @@ import org.jbox2d.common.*;
 import org.jbox2d.collision.shapes.*;
 import java.awt.Color;
 
-public class Platform implements DrawableBody {
+public class Square implements DrawableBody {
 
 	private Body body;
 	private Fixture fixture;
 	private Color color;
 	
-	public Platform(World world) {
+	public Square(World world) {
 		BodyDef bdef = new BodyDef();
-		bdef.type = BodyType.STATIC;
-		bdef.position.set(0,1);
-		body = world.createBody(bdef);
-
-		body.setTransform(new Vec2(-4, 4), 0);
+		bdef.type = BodyType.DYNAMIC;
+		bdef.position.set(x,10);
+		body = model.world.createBody(bdef);
 		
 		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(4, 1);
+		shape.setAsBox(.5f, .5f);
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = shape;
-		fixtureDef.density=1;
-		fixtureDef.friction=0.3f;
+		fixtureDef.density = 1;
+		fixtureDef.friction = 0.3f;
 		this.fixture = body.createFixture(fixtureDef);
-		//body.setUserData(f); //make this better
 
-		this.color = Color.green;
+		this.color = Color.red;
 	}
 
 	public Color getColor() {
