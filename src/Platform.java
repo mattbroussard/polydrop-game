77797f9d4fa6e923c1c1a63934275950437553c1
Josh Kelle <joshua.a.kelle@gmail.com>
2014-02-22 17:30:25 -0600

    Platform class implements DrawableBody

--- src/Platform.java
+++ src/Platform.java
@@ -1,43 +1,38 @@
-import java.awt.Polygon;
 
-public class Platform extends Block{
+public class Platform implements DrawableBody {
+
+	private Body body;
+	private Color color;
 	
-	public Platform(int x, int y, int w, int h){
-		super();
-		this.addPoint(x  , y  );
-		this.addPoint(x+w, y  );
-		this.addPoint(x+w, y+h);
-		this.addPoint(x  , y+h);
-	
-	}
-	
-	public void moveLeft(double d){
-		for(int i = 0; i < xpoints.length; i++)
-			xpoints[i] -= d;
-	}
-	public void moveRight(double d){
-		moveLeft(d*-1);		
-	}
-	public void tilt(double theta){
+	public Platform(int x, int y, int w, int h) {
+		BodyDef bdef = new BodyDef();
+		bdef.type = BodyType.STATIC;
+		bdef.position.set(0,1);
+		body = world.createBody(bdef);
+
+		body.setTransform(new Vec2(-4, 4), 0);
 		
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(4, 1);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.density=1;
+		fixtureDef.friction=0.3f;
+		Fixture f = body.createFixture(fixtureDef);
+		body.setUserData(f); //make this better
+
+		this.color = Color.green;
 	}
 
-	public Vector2 getCenterOfMass() {
-		return new Vector2(0.0, 0.0);
-	}
-	public Vector2 addForce() {
-		return new Vector2(0.0, 0.0);
-	}
-	public Vector2 getNetForce() {
-		return new Vector2(0.0, 0.0);
-	}
-	public void applyNetForce() {
-		// do this
+	public Color getColor() {
+		return this.color;
 	}
 
-	@Override
-	public void rotate(double theta) {
-		// TODO Auto-generated method stub
-		
+	public Body getBody() {
+		return this.body;
+	}
+
+	public getFixture() {
+		return this.body.getUserData();
 	}
 }
