85810d980c4eabe9e3761fcc7cb5edbda9b37234
Matt Broussard <mattb@cs.utexas.edu>
2014-02-22 17:11:05 -0600

    added drawablebody interface

---
+++ src/DrawableBody.java
@@ -0,0 +1,11 @@
+
+import org.jbox2d.dynamics.*;
+import java.awt.Color;
+
+public interface DrawableBody {
+	
+	public Body getBody();
+	public Fixture getFixture();
+	public Color getColor();
+
+}
