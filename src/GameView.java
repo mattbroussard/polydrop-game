483e5b927f92eeac7ee88d8eaf7f5e462f620c2f
DKelle <derplyfe@gmail.com>
2014-02-22 01:59:16 -0600

    simple platform can move right

--- src/GameView.java
+++ src/GameView.java
@@ -1,25 +1,58 @@
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 
-public class GameView extends JComponent {
+public class GameView extends JComponent implements KeyListener{
 	
 	GameModel model;
-	
+	GameController controller;
+	boolean pressedRight;
+	boolean pressedPause;
 
-	public GameView(GameModel m) {
+	public GameView(GameModel m, GameController c) {
 
 		super();
 		model = m;
-
+		controller = c;
 	}
 
+	
+
+	@Override
+	public void keyPressed(KeyEvent e) {		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT && !pressedRight){
+			pressedRight = true;
+			controller.movePlatformRight(10);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SPACE && !pressedPause){
+			controller.pause();
+			pressedPause = true;
+		}
+			
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) pressedRight = false;
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) pressedPause = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
 		g.drawString("Hello World", 20, 20);
 		
 		g.setColor(Color.GRAY);
-		g.fill3DRect((int)(model.x), (int)(model.y), (int)(model.w), (int)(model.h), true);
+		Platform p = model.getPlatform();
+		g.fillPolygon(p);
 		
 	}
 
