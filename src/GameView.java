08d8ebc1bc764bccfaf73eae3deb71a7c985ae47
DKelle <derplyfe@gmail.com>
2014-02-21 23:27:47 -0600

    Draws a rectangle

--- src/GameView.java
+++ src/GameView.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class GameView extends JComponent {
 	
 	GameModel model;
+	
+	int x, y, w, h;
 
 	public GameView(GameModel m) {
 
@@ -16,7 +18,10 @@ public class GameView extends JComponent {
 	public void paintComponent(Graphics g) {
 
 		g.drawString("Hello World", 20, 20);
-
+		
+		g.setColor(Color.GRAY);
+		g.fill3DRect(x, y, w, h, true);
+		
 	}
 
 }
