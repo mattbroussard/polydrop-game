5cd96ee75000ffeac4e46a24d47795caa1cf74bf
Josh Kelle <joshua.a.kelle@gmail.com>
2014-02-22 22:00:47 -0600

    removed Block.java

--- src/Block.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Polygon;
-public abstract class Block extends Polygon {
-	public abstract Vector2 getCenterOfMass();
-	public abstract Vector2 addForce();
-	public abstract Vector2 getNetForce();
-	public abstract void rotate(double theta);
-}
