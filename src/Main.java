3e87212f6b0d8febe1c8c2f3071b3e3525583444
Matt Broussard <mattb@cs.utexas.edu>
2014-02-23 10:53:04 -0600

    attempt to hide mouse cursor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 
 public class Main {
 
@@ -15,11 +16,21 @@ public class Main {
 		GameController game = new GameController(model);
 		LeapController leap = new LeapController(game);
 
-		//Setup view
+		//Mac thing for full screen mode, apparently has to be set before making the frame
+		System.setProperty("apple.awt.fullscreenhidecursor", "true");
+
+		//Setup view and frame
 		GameView view = new GameView(model, game);
 		game.addView(view);
 		JFrame frame = new JFrame("Derp");
+		frame.setContentPane(view);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(view);
 
+		//Set cursor to invisible
+		view.setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB), new Point(), "null"));
+
+		//Setup full screen window
 		if (FULL_SCREEN) {
 
 			frame.setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -40,9 +51,7 @@ public class Main {
 
 		}
 
-		frame.setContentPane(view);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.addKeyListener(view);
+		//Make window visible
 		frame.setVisible(true);
 
 	}
