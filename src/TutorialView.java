3d416ce9a4a5f9d92d4da82773745143604b154c
DKelle <Dallas.Kelle@gmail.com>
2014-04-07 18:03:21 -0500

    Tutorial now more indepth, while taking less time to complete

--- src/TutorialView.java
+++ src/TutorialView.java
@@ -5,31 +5,46 @@ import java.util.Iterator;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
 
+
+
+class Instruction{
+	float x, y;
+	String description;
+	
+	public Instruction(float x, float y, String description){
+		this.x = x;
+		this.y = y;
+		this.description = description;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+}
 public class TutorialView extends View  implements RadialMenuListener{
 	
 	ArrayList<DrawableBody> blockList = new ArrayList<DrawableBody>();
 	
 	
-	static final int TUTORIAL_MENU_PLAY = 0;
-	static final int TUTORIAL_MENU_ABOUT = 1;
-	static final int TUTORIAL_MENU_EXIT_GAME = 2;
+	static final int TUTORIAL_MENU_EXIT = 0;
 	
 	static final int MOVE_PLATFORM = 0;
 	static final int LINE_UP_PLATFORM = 1;
 	static final int HOLD_POLY = 2;
-	static final int SCORE_ONE = 3;
-	static final int SCORE_TWO = 4;
-	static final int HEALTH_ONE = 5;
-	static final int HEALTH_TWO = 6;
-	static final int HEALTH_THREE = 7;
-	static final int LEVEL_INDICATOR = 8;
-	static final int PAUSE = 9;
-	static final int UNPAUSE = 10;
-	static final int PAUSE_AGAIN = 11;
-	static final int MOVE_CURSOR = 12;
-	static final int LINE_UP_CURSOR = 13;
-	static final int SELECT_PLAY = 14;
-	static final int GAMEOVER = 15;
+	static final int SCORE_EXPLANATION_ONE = 3;
+	static final int SCORE_EXPLANATION_TWO = 4;
+	static final int HEALTH_EXPLANATION_ONE = 5;
+	static final int HEALTH_EXPLANATION_TWO = 6;
+	static final int LEVEL_INDICATOR_EXPLANATION_ONE = 7;
+	static final int LEVEL_INDICATOR_EXPLANATION_TWO = 8;
+	static final int RECEIVED_POINTS = 9;
+	static final int PAUSE = 10;
+	static final int UNPAUSE = 11;
+	static final int PAUSE_AGAIN = 12;
+	static final int MOVE_CURSOR = 13;
+	static final int LINE_UP_CURSOR = 14;
+	static final int SELECT_PLAY = 15;
+	static final int GAMEOVER = 16;
 	
 	static final int PAUSE_DELAY = 100;
 	
@@ -38,18 +53,15 @@ public class TutorialView extends View  implements RadialMenuListener{
 	
 	long pauseTimer = 0;
 	long time;
-	long squareSpawnTime;
 	long healthIncrease;
 	long startedLevel;
 	
 	World world;
-	Platform p;
+	Platform platform;
 
-	private int level = 0;
+	private int instructionNumber = PAUSE;
 	
 	float dx = 0;
-	float stringX = 0;
-	float stringY = 0;
 	float fontSize = .5f;
 	
 	Color stringColor = Colors.PLATFORM;
@@ -59,23 +71,24 @@ public class TutorialView extends View  implements RadialMenuListener{
 	private boolean paused;
 
 	
-	private String[] instructions = {	
-			"Move your hand to move the platform!",
-			"Line the platform up here",							
-			"Hold the poly until it dissapears!",
-			"This will tell you your score",
-			"Your score will increase everytime you catch a poly",
-			"This will tell you how much health you have left",
-			"Your health will decrease every time you drop a poly",
-			"Your health regenerates with time",
-			"This tells you how close you are to leveling up based on your score",
-			"Make a fist to pause the game",							
-			"Unclench your fist to unpause",							
-			"Pause again",												
-			"Now stick out your pointer finger to control the cursor",	
-			"Move the cursor here",										
-			"Drag the cursor to the left until you start the game!",
-			"Don't be that guy"};	
+	private Instruction[] instructions = {	
+			new Instruction(8f, 5f, "Move your hand to move the platform!"),
+			new Instruction(4.5f, 6.25f, "Line the platform up here"),							
+			new Instruction(8f, 5f, "Catch the poly!"),
+			new Instruction(8f, 5f, "If you hold it long enough, it will increase your score"),
+			new Instruction(8f, 5f, "But if you drop it, it will decrease your score and your health"),
+			new Instruction(9.25f, .825f, "This is your health bar"),
+			new Instruction(8f,5f,"Your health will regenerate with time"),
+			new Instruction(8f,2.5f,"This tells you how close you are to leveling up based on your score"),
+			new Instruction(8f,5f,"When you level up, shapes begin to fall faster"), //Thought about saying 'more frequently', but I think people will understand, and this sounds better
+			new Instruction(8f,5f, "Yay! You got points!"),
+			new Instruction(8f,5f, "Now, make a fist to pause the game"),							
+			new Instruction(8f,5f,"Unclench your fist to unpause"),							
+			new Instruction(8f,5f,"Pause again"),												
+			new Instruction(6.25f,5f, "Now stick out your pointer finger to control the cursor"),	
+			new Instruction(12f,1.5f,"Move the cursor here"),										
+			new Instruction(6.25f,5f,"Drag the cursor to the left to go back!"),
+			new Instruction(8f, 6f,"Don't be that guy") };	
 	
 
 	
@@ -84,82 +97,59 @@ public class TutorialView extends View  implements RadialMenuListener{
 		
 		menu = new RadialMenu(16.5f, 5f, this);
 
-		menu.addItem(new RadialMenuItem(TUTORIAL_MENU_PLAY, "Play", "singleMode", 163, 34, RadialMenuItem.ORIENT_LEFT, Colors.SPLASH_MENU_PLAY_SELECTED, Colors.SPLASH_MENU_PLAY_ACTIVE));
-		menu.addItem(new RadialMenuItem(TUTORIAL_MENU_ABOUT, "About", "about", 143, 20, RadialMenuItem.ORIENT_LEFT));
-		menu.addItem(new RadialMenuItem(TUTORIAL_MENU_EXIT_GAME, "Exit Game", "exit", 197, 20, RadialMenuItem.ORIENT_LEFT));
+		menu.addItem(new RadialMenuItem(TUTORIAL_MENU_EXIT, "Back", "menuReturn", 163, 34, RadialMenuItem.ORIENT_LEFT));
+
+		menu.setActiveItem(TUTORIAL_MENU_EXIT);
 		
-		menu.setActiveItem(TUTORIAL_MENU_PLAY);
-		
-		p = new Platform(world, Platform.FULL);
+		platform = new Platform(world, Platform.FULL);
 		time = System.currentTimeMillis();
 	}
 
 	public void draw(GraphicsWrapper g2, boolean active) {
 		
+		if(!active){
+			return;
+		}
+		
 		Color bg = isPaused() ? Colors.PAUSED : Colors.BACKGROUND;
 		g2.fillRect(0f, 0f, 16f, 10f, bg);
 		
-		switch(level){
-		case MOVE_PLATFORM:	
-			setStringCoord(8f,5f);
+		switch(instructionNumber){
+		case MOVE_PLATFORM:
+			g2.drawStringCentered("And make sure your fingers are spread!", .5f, stringColor, 8f, 6f);
 			break;
 		case LINE_UP_PLATFORM:
-			setStringCoord(4.5f,6.25f);
 			g2.fillRect(.5f, 5, 8, .5f, isPaused() ? Colors.BACKGROUND : Colors.PAUSED);
 			break;
-		case HOLD_POLY:
-			setStringCoord(8f,5f);
-			break;
-		case SCORE_ONE:
-			setStringCoord(3f,2.5f);
+/*		case SCORE_ONE:
 			g2.fillRect(.5f, 1.25f, 3.5f, .125f, Colors.HEALTH_GOOD);
 			break;
 		case SCORE_TWO:
-			setStringCoord(5.75f,2.5f);
 			g2.fillRect(.5f, 1.25f, 3.5f, .125f, Colors.HEALTH_GOOD);
+			break;*/
+		case HEALTH_EXPLANATION_ONE:
+		case HEALTH_EXPLANATION_TWO:
+			//g2.fillRect(11.5f, 1.5f, 3, .125f, Colors.HEALTH_GOOD);
 			break;
-		case HEALTH_ONE:
-			setStringCoord(10f,2.5f);
-			g2.fillRect(11.5f, 1.5f, 3, .125f, Colors.HEALTH_GOOD);
-			break;
-		case HEALTH_TWO:
-			setStringCoord(10f,2.5f);
-			g2.fillRect(11.5f, 1.5f, 3, .125f, Colors.HEALTH_GOOD);
-			break;
-		case HEALTH_THREE:
-			setStringCoord(11f,2.5f);
-			g2.fillRect(11.5f, 1.5f, 3, .125f, Colors.HEALTH_GOOD);
-			break;
-		case LEVEL_INDICATOR:
-			setStringCoord(8f,2.5f);
-			g2.fillRect(14.25f,1.75f,1.5f,.125f, Colors.HEALTH_GOOD);
-			break;
-		case PAUSE:
-			setStringCoord(8f,5f);
-			break;
-		case UNPAUSE:
-			setStringCoord(8f,5f);
-			break;
-		case PAUSE_AGAIN:
-			setStringCoord(8f,5f);
-			break;		
-		case MOVE_CURSOR:
-			setStringCoord(6.25f,5f);
+		case LEVEL_INDICATOR_EXPLANATION_ONE:
+		case LEVEL_INDICATOR_EXPLANATION_TWO:
+			//g2.fillRect(14.25f,1.75f,1.5f,.125f, Colors.HEALTH_GOOD);
 			break;
 		case LINE_UP_CURSOR:
-			setStringCoord(12f,1.5f);
-			g2.fillCircle(15, 5, .25f, Colors.LEAP_WARNING_OVERLAY);
+			g2.maskCircle(15, 5, .25f);
+			g2.fillCircle(15, 5, .5f, Colors.LEAP_WARNING_OVERLAY);
 			break;
 		case SELECT_PLAY:
-			setStringCoord(6.25f,5f);
 			g2.fillRect(12, 5, 3, .125f, Colors.LEAP_WARNING_OVERLAY);
 			break;
 		case GAMEOVER:
-			setStringCoord(8f, 6f);
 			g2.drawStringCentered("GAME OVER", 2.55f, Colors.HEALTH_BAD, 8f, 4f);
+			break;
+			default:
+				break;
 		}
 		
-		if(level >= HOLD_POLY){
+		if(instructionNumber >= HOLD_POLY){
 			//Draw score
 			TextRenderer.drawScore(g2, score);
 
@@ -171,16 +161,19 @@ public class TutorialView extends View  implements RadialMenuListener{
 			
 		}
 		
-		g2.drawStringCentered(instructions[level], fontSize, stringColor, stringX, stringY);
+		g2.drawStringCentered(instructions[instructionNumber].description, fontSize, stringColor, instructions[instructionNumber].x, instructions[instructionNumber].y);
 		
 		if(!isPaused()){
 			physicsUpdate(System.currentTimeMillis());
-		}else if(level > PAUSE_AGAIN && level != GAMEOVER){
-			menu.draw(g2);
+		}else{
+			TextRenderer.drawPaused(g2);
+			if(instructionNumber > PAUSE_AGAIN && instructionNumber != GAMEOVER){
+				menu.draw(g2);
+			}
 		}
 		instructionUpdate(System.currentTimeMillis());
 		
-		BodyRenderer.drawBody(p, g2, isPaused());
+		BodyRenderer.drawBody(platform, g2, isPaused());
 		
 		for(DrawableBody db : blockList){
 			BodyRenderer.drawBody(db, g2, isPaused());
@@ -189,12 +182,7 @@ public class TutorialView extends View  implements RadialMenuListener{
 		g2.prepare();
 
 	}
-	
-	public void setStringCoord(float x, float y){
-		stringX = x;
-		stringY = y;
-	}
-	
+
 	public void pause(boolean paused){
 		
 		if(paused){
@@ -217,7 +205,7 @@ public class TutorialView extends View  implements RadialMenuListener{
 			pauseTimer = 0;
 
 		}
-		if(level == SELECT_PLAY){
+		if(instructionNumber >= LINE_UP_CURSOR){
 			return;
 		}
 		this.paused = paused;
@@ -229,7 +217,7 @@ public class TutorialView extends View  implements RadialMenuListener{
 
 		world.step((now-time)/1000f, 6, 2);
 		
-		if(now - healthIncrease > 3 * 1000 && level != GAMEOVER){
+		if(now - healthIncrease > 3 * 1000 && instructionNumber != GAMEOVER){
 			health = Math.min(health+3, 100);
 			healthIncrease = now;
 		}
@@ -238,13 +226,14 @@ public class TutorialView extends View  implements RadialMenuListener{
 		while( itr.hasNext() ) {
 			DrawableBody b = itr.next();
 			long dt = now - time;
-			b.reduceLifetime(dt);
+			b.reduceLifetime((long) (dt*(10/(3.5*7)))); // this is to increase the amount of time it takes the block to dissapear, so the user is still doing something when the explanation for the radial level indicator is being explained
 			if( b.getExpiration() <= 0 ) {
 				// remove block
 				itr.remove();
 				world.destroyBody(b.getBody());
 				score += b.getReward();
-				levelUp(now);
+				SoundManager.play("pointGain");
+				//levelUp(now);
 			}
 			Vec2 pos = b.getBody().getPosition();
 			if(pos.y < -2) {
@@ -252,12 +241,13 @@ public class TutorialView extends View  implements RadialMenuListener{
 				score += b.getPenalty();
 				health = Math.max(0, health-5);
 				if(health == 0){
-					level = GAMEOVER;
-					startedLevel = now;
+					setInstructionNumber(GAMEOVER, now);
 				}else{
-					instructions[level] = "Try again!";
+					setInstructionNumber(HOLD_POLY, now);
+					instructions[instructionNumber].setDescription("Try again"); 
 				}
 				itr.remove();
+				SoundManager.play("pointLoss");	
 				world.destroyBody(b.getBody());
 
 			}
@@ -266,8 +256,13 @@ public class TutorialView extends View  implements RadialMenuListener{
 		time = System.currentTimeMillis();
 	}
 	
+	public void setInstructionNumber(int num, long now){
+		startedLevel = now;
+		instructionNumber = num;
+	}
+	
 	public void instructionUpdate(long now){
-		switch(level){
+		switch(instructionNumber){
 			case MOVE_PLATFORM:
 				if(dx > 15f){
 					levelUp(now);
@@ -276,49 +271,28 @@ public class TutorialView extends View  implements RadialMenuListener{
 				break;
 			case LINE_UP_PLATFORM:
 				
-				if(	Math.abs(p.getBody().getPosition().x + 4 - .5) < 1
-				 && Math.abs(10 - p.getBody().getPosition().y + .5 - 5) < 1){
+				if(	Math.abs(platform.getBody().getPosition().x + 4 - .5) < 1
+				 && Math.abs(10 - platform.getBody().getPosition().y + .5 - 5) < 1){
 					levelUp(now);
 				}
 				break;
 			case HOLD_POLY:
 				if(blockList.size() < 1){
 					blockList.add(spawn(-8 + 4.5f));
-					blockList.get(0).reduceLifetime(5);
 				}
-
-				break;	
-			case SCORE_ONE:
-				if(now - startedLevel > 5 * 1000){
-					levelUp(now);
-				}
-				break;
-			case SCORE_TWO:
-				if(now - startedLevel > 5 * 1000){
-					levelUp(now);
-				}
-				break;
-			case HEALTH_ONE:
-				if(now - startedLevel > 5 * 1000){
-					levelUp(now);
-				}
-				break;
-			case HEALTH_TWO:
-				if(now - startedLevel > 5 * 1000){
-					levelUp(now);
-				}
-				break;
-			case HEALTH_THREE:
-				if(now - startedLevel > 5 * 1000){
-					levelUp(now);
-				}
-				break;
-			case LEVEL_INDICATOR:
-				if(now - startedLevel > 5 * 1000){
+			case SCORE_EXPLANATION_ONE:
+			case SCORE_EXPLANATION_TWO:
+			case HEALTH_EXPLANATION_ONE:
+			case HEALTH_EXPLANATION_TWO:
+			case LEVEL_INDICATOR_EXPLANATION_ONE:
+			case LEVEL_INDICATOR_EXPLANATION_TWO:
+			case RECEIVED_POINTS:
+				if(now - startedLevel > 3.5 * 1000){
 					levelUp(now);
 				}
 				break;
 			case PAUSE:
+			case PAUSE_AGAIN:
 				if(isPaused()){
 					levelUp(now);
 				}
@@ -328,11 +302,7 @@ public class TutorialView extends View  implements RadialMenuListener{
 					levelUp(now);
 				}
 				break;
-			case PAUSE_AGAIN:
-				if(isPaused()){
-					levelUp(now);
-				}
-				break;
+
 			case MOVE_CURSOR:
 				if(dx > 275){
 					dx = 0;
@@ -350,10 +320,10 @@ public class TutorialView extends View  implements RadialMenuListener{
 				break;
 			case GAMEOVER:
 				if(now - startedLevel > 5 * 1000){
-					level = 0;
+					instructionNumber = 0;
 					score = 0;
 					health = 35;
-					instructions[HOLD_POLY] = "Hold the poly until it dissapears!";
+					instructions[HOLD_POLY].setDescription("Hold the poly until it dissapears!"); 
 				}
 				break;
 
@@ -363,24 +333,24 @@ public class TutorialView extends View  implements RadialMenuListener{
 	
 	public void levelUp(long now){
 		startedLevel = now;
-		level++;
+		instructionNumber++;
 	}
 	
 	public void updatePlatform(double handx, double handy, double theta, long dt){
 
 		if (handx != -1) {
 			System.out.println("dx: "+this.dx);
-			double dtheta = theta - p.getBody().getAngle();
-			double dx = (16*(float)handx - 8) - p.getBody().getPosition().x;
-			double dy = (10*(float)handy)     - p.getBody().getPosition().y;
-			p.getBody().setLinearVelocity(new Vec2((float)(dx/dt*1000), (float)(dy/dt*1000)));
-			p.getBody().setAngularVelocity((float)(dtheta/dt*1000));
-			if(level == MOVE_PLATFORM){
+			double dtheta = theta - platform.getBody().getAngle();
+			double dx = (16*(float)handx - 8) - platform.getBody().getPosition().x;
+			double dy = (10*(float)handy)     - platform.getBody().getPosition().y;
+			platform.getBody().setLinearVelocity(new Vec2((float)(dx/dt*1000), (float)(dy/dt*1000)));
+			platform.getBody().setAngularVelocity((float)(dtheta/dt*1000));
+			if(instructionNumber == MOVE_PLATFORM){
 				this.dx  += handx;
 			}
 		} else {
-			p.getBody().setLinearVelocity(new Vec2(0f, 0f));
-			p.getBody().setAngularVelocity(0f);
+			platform.getBody().setLinearVelocity(new Vec2(0f, 0f));
+			platform.getBody().setAngularVelocity(0f);
 		}
 	
 	}
@@ -390,27 +360,15 @@ public class TutorialView extends View  implements RadialMenuListener{
 	}
 	
 	public DrawableBody spawn(float x) {
-		int sides = (int)Math.round(Math.random()*5) + 3;
-
-		int newPoly = (int)(Math.random()*5)+3;
-		
-		System.out.println("spawing at "+x);
-		return new PolyBody(world, x, newPoly, GameModel.ONE_HAND);
+		int numberOfSides = (int)(Math.random()*5)+3;
+		return new PolyBody(world, x, numberOfSides, GameModel.ONE_HAND);
 	}
 
 	public void onMenuSelection(int id) {
 
 		switch (id) {
-			case TUTORIAL_MENU_PLAY:
-				getViewManager().pushView("paused");
-				break;
-
-			case TUTORIAL_MENU_ABOUT:
-				getViewManager().pushView("about");
-				break;
-
-			case TUTORIAL_MENU_EXIT_GAME:
-				System.exit(0);
+			case TUTORIAL_MENU_EXIT:
+				getViewManager().popView();
 				break;
 
 			default:
@@ -420,10 +378,11 @@ public class TutorialView extends View  implements RadialMenuListener{
 		SoundManager.play("menuChoice");
 
 	}
+	
 	public void pointerUpdate(float cursorX, float cursorY) {
-		if(level > PAUSE_AGAIN){
+		if(instructionNumber > PAUSE_AGAIN){
 			menu.pointerUpdate(cursorX, cursorY);
-			if(level == MOVE_CURSOR){
+			if(instructionNumber == MOVE_CURSOR){
 				this.dx += cursorX;
 			}
 		}
